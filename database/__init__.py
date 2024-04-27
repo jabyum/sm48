181aee70f40e75adf2895380432e0b4e0a8e908c
jabyum <jabyum@github.com>
2024-04-27 19:01:12 +0500

    last

--- database/__init__.py
+++ database/__init__.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:admin@localhost/social_media"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@database/postgres"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
