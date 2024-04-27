e21db7e8adb9c50147abc64e79193db7852f6d6a
jabyum <jabyum@github.com>
2024-04-27 18:08:30 +0500

    last

--- api/hashtag_api/hashtag.py
+++ api/hashtag_api/hashtag.py
@@ -1,12 +1,21 @@
 from fastapi import Request, APIRouter
 from database.postservice import get_exact_hashtag_db, get_some_hashtags_db
-hashtag_router = APIRouter(prefix='/hashtag', tags=['Управление хэштегами'])
 
-# получить хэщтеги
+hashtag_router = APIRouter(prefix='/hashtag', tags=['Управление хештегами'])
 @hashtag_router.get('/api')
 async def get_some_hashtags(size: int = 20, page: int = 1):
-    pass
-# получить хэштег
-@app.get('/api/hastag/<str:hashtag_name>')
-async def get_exact_hashtag(request: Request):
-    pass
+    if size and page:
+        exact_hashtag = get_some_hashtags_db(size)
+        return {'status': 1, 'message': exact_hashtag}
+    return {'status': 0, 'message': 'wrong data entry'}
+
+
+# @hashtag_router.get('/api/hastag/<str:hashtag_name>')
+# async def get_exact_hashtag(request: Request):
+#     data = await request.json()
+#     hashtag_name = data.get('hashtag_name')
+#
+#     if hashtag_name:
+#         exact_hashtag = get_exact_hashtag_db(hashtag_name)
+#         return {'status': 1, 'message': exact_hashtag}
+#     return {'status': 0, 'message': 'wrong data entry'}
--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI
 from database import Base, engine
 from api.users_api.users import user_router
 from api.photo_api.photo import photo_router
+from api.posts_api.post import post_router
+from api.comments_api.comments import comment_router
+from api.hashtag_api.hashtag import hashtag_router
 from fastapi.staticfiles import StaticFiles
 # команда для создания всех таблиц в дб
 Base.metadata.create_all(bind=engine)
@@ -9,4 +12,7 @@ app = FastAPI(docs_url="/")
 app.mount("/static", StaticFiles(directory="static"))
 app.include_router(user_router)
 app.include_router(photo_router)
+app.include_router(post_router)
+app.include_router(comment_router)
+app.include_router(hashtag_router)
 
